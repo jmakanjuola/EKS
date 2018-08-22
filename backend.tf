1a3dd8ca2d72ea232fce35bc6547a29a60e6f26f
Jubilee Makanjuola <jmakanjuola@users.noreply.github.com>
2018-08-22 12:11:36 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
-# "terraform" {
-#   backend "s3" {
-#     bucket = "earnest-eks-state"
-#     key    = "development1.8/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-#
-#   tags {
-#     Name = "earnest-eks-state"
-#   }
-# }
 
-=======
 "terraform" {
   backend "s3" {
     bucket = "eks-terraform-state"
@@ -19,4 +6,4 @@
     region = "us-west-2"
   }
 }
->>>>>>> 5dc3eb4643346f9b444b0bb50ece321158145b55
+
