31585b8de73402a22e44b749d225f263dbf986dc
Jubilee Makanjuola <jmakanjuola@users.noreply.github.com>
2018-08-22 12:16:41 -0700

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,18 +1,5 @@
 # This resource is the actual Kubernetes master cluster
 
-<<<<<<< HEAD
-resource "aws_eks_cluster" "earnest-eks-cluster" {
-  name     = "${var.earnest-eks-cluster-name}"
-  role_arn = "${aws_iam_role.earnest-eks-cluster.arn}"
-
-  vpc_config {
-    security_group_ids = ["${aws_security_group.earnest-eks-cluster.id}"]
-
-    subnet_ids = [
-      "${aws_subnet.eks-private-subnet-1a.id}",
-      "${aws_subnet.eks-private-subnet-1b.id}",
-      "${aws_subnet.eks-private-subnet-1c.id}",
-=======
 resource "aws_eks_cluster" "demo" {
   name     = "${var.cluster-name}"
   role_arn = "${aws_iam_role.demo-cluster.arn}"
@@ -24,17 +11,12 @@ resource "aws_eks_cluster" "demo" {
       "${aws_subnet.eks-private-subnet-2a.id}",
       "${aws_subnet.eks-private-subnet-2b.id}",
       "${aws_subnet.eks-private-subnet-2c.id}",
->>>>>>> 5dc3eb4643346f9b444b0bb50ece321158145b55
     ]
   }
 
   depends_on = [
-<<<<<<< HEAD
-    "aws_iam_role_policy_attachment.earnest-eks-cluster-AmazonEKSClusterPolicy",
-    "aws_iam_role_policy_attachment.earnest-eks-cluster-AmazonEKSServicePolicy",
-=======
     "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSClusterPolicy",
     "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSServicePolicy",
->>>>>>> 5dc3eb4643346f9b444b0bb50ece321158145b55
+
   ]
 }
